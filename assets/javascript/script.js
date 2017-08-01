e9cfdb1e8e1a62d7ebd7260ffee3966a728c1aac
Jay Sullivan <JaySull514@gmail.com>
2017-07-31 22:43:40 -0500

    Fixed js finally and messed with styles

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,5 @@
 $('#submit-btn').on('click', function() {
 	event.preventDefault();
 	var currentInput = $('#current-input').val();
-	var newBtn = $('<button>');
-	newBtn.addClass('btn');
-	newBtn.append(currentInput);
-	$('#btn_container').append(newBtn);
+	$('.btn-container').append('<button class="btn" value="' + currentInput + '">' + currentInput + '</button>');
 });
