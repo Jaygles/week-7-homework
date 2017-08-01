9e14e899567ed12b69bfdfe5108b84ddbea9c453
Jay Sullivan <JaySull514@gmail.com>
2017-08-01 10:30:51 -0500

    Added basic functionality for dynamically created buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,18 @@
 $('#submit-btn').on('click', function() {
 	event.preventDefault();
 	var currentInput = $('#current-input').val();
-	$('.btn-container').append('<button class="btn" value="' + currentInput + '">' + currentInput + '</button>');
+	searchValue = currentInput.replace(/ /g,"+");
+	$('.btn-container').append('<button class="btn search-btn" value="' + searchValue + '">' + currentInput + '</button>');
+});
+
+$(document).on('click', '.search-btn', function() {
+	var searchTerm = $(this).attr('value');
+	console.log(searchTerm);
+	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=5");
+	xhr.done(function(result) {
+		console.log(result);
+		for (var i = result.data.length - 1; i >= 0; i--) {
+			$('.gif-display-wrapper').append('<img src="' + result.data[i].images.fixed_width.url + '">');
+		};	
+	});
 });
